638966e1db00534ca3745f81e39d9523f71e28fa
Karolis Petrukonis <karolispetrukonis@yahoo.com>
2021-05-09 19:26:37 +0300

    Final changes with the working code

--- main.js
+++ main.js
@@ -83,6 +83,99 @@
 
 
 
+//New Version with "cleaner code"
+
+//declare a variable for score
+//both computer and player start with zero
+let playerScore = 0;
+let computerScore = 0;
+
+//Once again, function that describes how computer is making decission
+function computerPlay() {
+
+    const computerChoices = ['rock', 'paper', "scissors"];
+    let random = Math.floor(Math.random() * 3);
+    return computerChoices[random];
+}
+
+//now a function that asks a human player to input his or her choice
+function playerPlay() {
+    const playerChoice = prompt('Please enter your value');
+    const option = playerChoice.toLowerCase();
+    return option;
+}
+
+
+//function that plays one round of the game
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return 'It is a tie';
+    }
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            playerScore++;
+            return 'Player wins'
+        } else {
+            computerScore++;
+            return 'Computer wins';
+        }
+    }
+
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            playerScore++;
+            return 'Player wins';
+        } else {
+            computerScore++;
+            return 'Computer wins!';
+        }
+    }
+
+    if (playerSelection === 'scissors') {
+        if (computerSelection === "paper") {
+            playerScore++;
+            return 'Player wins';
+        } else {
+            computerScore++;
+            return 'Computer wins';
+        }
+    }
+}
+
+// let playerSelection = playerPlay();
+// let computerSelection = computerPlay();
+
+// //test one round wit console.log to see if it works
+// console.log(playRound(playerSelection, computerSelection));
+
+
+
+//function that runs the game five times and declares a 
+function game() {
+    for (let i = 0; i <= 4; i++) {
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        console.log('Computer chooses :' + computerSelection);
+        console.log('Player chooses :' + playerSelection);
+        console.log('Computer score :' + computerScore);
+        console.log('Player score :' + playerScore);
+    }
+    alert('Player final score: ' + playerScore);
+    alert('Computer final score: ' + computerScore);
+    if (playerScore > computerScore) {
+        alert('Player is the winner!');
+    }else if (computerScore > playerScore) {
+        alert('Computer is the winner!');
+    } else {
+        alert('It is a tie!');
+    }
+}
+
+game();
+
 
 
 
